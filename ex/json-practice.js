79d85e564caff92ad71f8b9ccef4f266e78e4b58
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-07-24 16:19:13 -0300

    Add js async examples such as callbacks and promises

--- ex/json-practice.js
+++ ex/json-practice.js
@@ -15,7 +15,7 @@ const section = document.querySelector('section')
 //   showHeroes(superHeroes)
 // }
 
-let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json'
+let requestURL = 'https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg'
 let request = new XMLHttpRequest()
 request.open('GET', requestURL)
 request.responseType = 'text'
